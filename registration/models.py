0c5e6feeb3bf9ff6eb429e8d7579961dbf431559
rushikesh24 <zrushi24@gmail.com>
2019-08-09 23:21:12 +0530

    Registration Error

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+
+class Year_marks(models.Model):
+    FE = models.DecimalField(max_digits=4, decimal_places=2,null=False,blank=False,default=0.00)
+    SE = models.DecimalField(max_digits=4, decimal_places=2,null=False,blank=False,default=0.00)
+    TE = models.DecimalField(max_digits=4, decimal_places=2,null=False,blank=False,default=0.00)
+    BE = models.DecimalField(max_digits=4, decimal_places=2,null=False,blank=False,default=0.00)
+    class Meta:
+        abstract = True
+
+class Student(models.Model):
+    pnr = models.CharField(max_length=10, null=False, blank=False, primary_key=True)
+    name = models.CharField(max_length=80,null=False,blank=False)
+    email = models.EmailField(max_length=50,null=False,blank=False)
+    birthdate = models.DateField(auto_now=False, auto_now_add=False)
+    tenth = models.DecimalField(max_digits=5, decimal_places=2)
+    diploma_12 = models.DecimalField(max_digits=5, decimal_places=2)
+    branch = models.CharField(max_length=50,null=False,blank=False)
+    gender = models.CharField(max_length=2,null=False,blank=False)
+    placed = models.BooleanField(default=False)
+    primary_mobile = models.CharField(max_length=10,null=False,blank=False)
+    secondary_mobile = models.CharField(max_length=10,blank=True)
+    attendence_status = models.BooleanField(default=False)
+    marks = models.ArrayModelField(model_container=Year_marks)
+    avg_marks = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Employee(models.Model):
+    id = models.CharField(max_length=10, null=False, blank=False,primary_key=True)
+    name = models.CharField(max_length=80, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False)
+    birthdate = models.DateField(auto_now=False, auto_now_add=False)
+    designation = models.CharField(max_length=50, null=False, blank=False)
+    gender = models.CharField(max_length=2, null=False, blank=False)
+    department = models.CharField(max_length=50,null=False,blank=False)
+    approved = models.BooleanField(default=False)
+    primary_mobile = models.CharField(max_length=10, null=False, blank=False)
+    secondary_mobile = models.CharField(max_length=10,blank=True)
+    password = models.CharField(max_length=16,null=False,blank=False)
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from pymongo.auth import authenticate
+
+
+def registration(request):
+    if request.method == 'POST':
+        print("Hello")
+        form = UserCreationForm(request.POST)
+        print(form)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('id_password1')
+            #print(str(username)+str(raw_password))
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        print('Hello World...')
+        form = UserCreationForm()
+    return render(request,'registration.html',{})
