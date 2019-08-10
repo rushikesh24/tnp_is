d13acae7b8f25d180fae6ab167ff5752cb6865d3
mayur2139 <mayurnagre22@gmail.com>
2019-08-10 17:11:01 +0530

    login perfectly working

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,HttpResponse
 
 # Create your views here.
 from pymongo.auth import authenticate
@@ -10,16 +10,12 @@ def registration(request):
     if request.method == 'POST':
         print("Hello")
         form = UserCreationForm(request.POST)
-        print(form)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('id_password1')
-            #print(str(username)+str(raw_password))
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
+        username = request.POST.get('username')
+        raw_password = request.POST.get('id_password1')
+        print(str(username)+str(raw_password))
+        #user = authenticate(username=username, password=raw_password)
+        #login(request, user)
+        return HttpResponse(username + raw_password)
     else:
         print('Hello World...')
-        form = UserCreationForm()
     return render(request,'registration.html',{})
